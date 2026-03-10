d5deda011f62a57fd3066b7edc45ea3b35ec7b5b
sasank-reddy <sasankreddy35@gmail.com>
2026-03-10 16:55:47 +0530

    Remove guide module from partner branch

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 
 from src.api.v1.endpoints import partner
-from src.api.v1.endpoints import guide
-
 app = FastAPI(title="AP Tourism Backend",version="1.0.0")
 
 # @app.get("/")
@@ -11,4 +9,3 @@ app = FastAPI(title="AP Tourism Backend",version="1.0.0")
 
 # include routers
 app.include_router(partner.router)
-app.include_router(guide.router)
