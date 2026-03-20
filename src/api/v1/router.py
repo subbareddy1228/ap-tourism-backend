b6462e71e2c02f54c288cb6e69615bafde6c2e08
PERUMALLA SUBBARAO <subbusv1228@gmail.com>
2026-03-20 09:12:13 +0530

    routers changed

--- src/api/v1/router.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from src.api.v1.endpoints import temple, darshan
-
-router = APIRouter()
-
-
-router.include_router(temple.router)
-router.include_router(darshan.router)
