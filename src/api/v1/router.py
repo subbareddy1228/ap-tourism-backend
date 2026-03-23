8f7b07103771867e540ba7b1c366db08c78bf704
PERUMALLA SUBBARAO <subbusv1228@gmail.com>
2026-03-23 17:29:26 +0530

    coupon integratin done

--- src/api/v1/router.py
+++ src/api/v1/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from src.api.v1.endpoints import auth, bookings, destination, guide, packages, payments, users, vehicles, wallet, partner, temple, darshan, hotels
+from src.api.v1.endpoints import auth, bookings, coupons, destination, guide, packages, payments, users, vehicles, wallet, partner, temple, darshan, hotels
 
 router = APIRouter()
 
@@ -16,3 +16,4 @@ router.include_router(destination.router)
 router.include_router(packages.router)
 router.include_router(bookings.router)
 router.include_router(payments.router)
+router.include_router(coupons.router)
