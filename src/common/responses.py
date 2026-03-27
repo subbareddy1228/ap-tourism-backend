5397aef37e815518fd4100bae74cabac4d08082d
PERUMALLA SUBBARAO <subbusv1228@gmail.com>
2026-03-27 17:51:29 +0530

    db

--- src/common/responses.py
+++ src/common/responses.py
@@ -3,8 +3,11 @@ common/responses.py
 Standard API response format used across all endpoints.
 """
 
+import code
 from typing import Any, Optional
 from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+
 
 
 
@@ -21,4 +24,22 @@ class APIResponse(BaseModel):
     @classmethod
     def error(cls, message: str = "Error", data: Any = None):
         return cls(status="error", message=message, data=data)
+    
+
+    def success_response(data=None, message="Success"):
+        return {
+        "success": True,
+        "message": message,
+        "data": data
+    }
+
+    def error_response(message="Error", code=400):
+        return JSONResponse(
+        status_code=code,
+        content={
+            "success": False,
+            "error": message,
+            "code": code
+        }
+    )
 
