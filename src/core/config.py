402c112695508f5970df20c404bc26a08e58616d
SHREEHARI285 <shreeharipappaka@gmail.com>
2026-04-22 16:21:55 +0530

    ignore (undifiend keys)

--- src/core/config.py
+++ src/core/config.py
@@ -82,7 +82,8 @@ class Settings(BaseSettings):
     # ── Load Environment Variables from .env ──────────────────
     model_config = SettingsConfigDict(
         env_file=".env",
-        case_sensitive=True
+        case_sensitive=True,
+        extra="ignore"
     )
 
 
