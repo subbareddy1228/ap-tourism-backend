462bee8a02364c9cb2ba0e3bf1c35df0f21186f1
raguanusha <anusharagu8@gmail.com>
2026-04-24 09:12:16 +0530

    changes

--- src/core/config.py
+++ src/core/config.py
@@ -20,13 +20,10 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+  
 
-    # ── Database ──────────────────────────────────────────────
-<<<<<<< HEAD
-    DATABASE_URL: str = "postgresql+asyncpg://postgres:Mounika@localhost:5432/ap_tourism"
-=======
     DATABASE_URL: str = "postgresql+asyncpg://postgres:12345@localhost:5432/ap_tourism"
->>>>>>> fe3bab894963d61ef32c53e32b2b0d36555bd77b
+# >>>>>>> fe3bab894963d61ef32c53e32b2b0d36555bd77b
 
     # ── Redis ─────────────────────────────────────────────────
     REDIS_URL: str = "redis://localhost:6379/0"
